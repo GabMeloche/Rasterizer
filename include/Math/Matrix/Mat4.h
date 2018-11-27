fd0bf9821bf7ac4b535355f563a8f78b8cf1f0c1
Lilian GADEAU <l.gadeau@student.isartdigital.com>
2018-11-27 16:02:36 -0500

    add copy constructor for Mat4

--- include/Math/Matrix/Mat4.h
+++ include/Math/Matrix/Mat4.h
@@ -1,17 +1,19 @@
 #pragma once
 
+#include <Math/Vector/Vec4.h>
+
 namespace Math::Matrix
 {
     class Mat4
     {
     public:
-
-        // ReSharper disable once IdentifierTypo
         float mf_Matrice4[4][4];
 
 		Mat4() = default;
 		~Mat4() = default;
+		Mat4(Mat4& p_Matrix);
 
-		Mat4& operator*(const Mat4& p_mat1);
+		Mat4& operator*(const Mat4& p_mat);
+        Mat4& operator*(const Vector::Vec4& p_vec)
 	};
 }
--- src/Math/Matrix/Mat4.cpp
+++ src/Math/Matrix/Mat4.cpp
@@ -2,7 +2,17 @@
 
 using namespace Math::Matrix;
 
-Mat4& Mat4::operator*(const Mat4 & p_mat1)
+Mat4::Mat4(Mat4& p_Matrix)
+{
+    for (int i = 0; i < 4; i++)
+    {
+		for (int j = 0; j < 4; j++)
+			mf_Matrice4[i][j] = p_Matrix.mf_Matrice4[i][j];
+    }
+}
+
+
+Mat4& Mat4::operator*(const Mat4 & p_mat)
 {
 	Mat4 m_mat3;
 
@@ -11,9 +21,17 @@ Mat4& Mat4::operator*(const Mat4 & p_mat1)
         for (int j = 0; j < 4; j++)
         {
 			for (int k = 0; k < 4; k++)
-				m_mat3.mf_Matrice4[i][j] += this->mf_Matrice4[i][k] * p_mat1.mf_Matrice4[k][j];
+				m_mat3.mf_Matrice4[i][j] += this->mf_Matrice4[i][k] * p_mat.mf_Matrice4[k][j];
         }
     }
 
 	return m_mat3;
 }
+
+Mat4& Mat4::operator*(const Vector::Vec4& p_vec)
+{
+	Vector::Vec4 tmp_Vec;
+
+    return tmp_Vec
+}
+

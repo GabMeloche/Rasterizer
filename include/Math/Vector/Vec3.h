2b2985b5b2c6ffe1435deff87acefc9f2ab00552
Victor JORE <v.jore@student.isartdigital.com>
2018-11-27 15:00:33 -0500

    Vec3 base code

---
+++ include/Math/Vector/Vec3.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace Math::Vector
+{
+	class Vec3
+	{
+
+	public:
+		Vec3();
+		~Vec3();
+
+		float mf_x;
+		float mf_y;
+		float mf_z;
+
+		float GetMagnitude() const;
+		void Normalize();
+
+		Vec3& operator*(float pf_scalar);
+		Vec3& operator+(Vec3& p_other);
+	};
+}
---
+++ src/Math/Vector/Vec3.cpp
@@ -0,0 +1,47 @@
+#include <Math/Vector/Vec3.h>
+
+
+using namespace Math::Vector;
+
+Vec3::Vec3()
+{
+	mf_x = 0;
+	mf_y = 0;
+	mf_z = 0;
+}
+
+Vec3::~Vec3()
+{
+}
+
+float Vec3::GetMagnitude() const
+{
+	return 0.0f;
+}
+
+void Vec3::Normalize()
+{
+}
+
+Vec3& Vec3::operator*(float pf_scalar)
+{
+	Vec3 p_tmp;
+
+	p_tmp.mf_x = mf_x * pf_scalar;
+	p_tmp.mf_x = mf_y * pf_scalar;
+	p_tmp.mf_x = mf_z * pf_scalar;
+
+	return p_tmp;
+
+}
+
+Vec3& Vec3::operator+(Vec3& p_other)
+{
+	Vec3 p_tmp;
+
+	p_tmp.mf_x = mf_x + p_other.mf_x;
+	p_tmp.mf_x = mf_y + p_other.mf_y;
+	p_tmp.mf_x = mf_z + p_other.mf_z;
+
+	return p_tmp;
+}

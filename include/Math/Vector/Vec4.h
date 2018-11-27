6a416bc7bec7668a56209b6f7425a977541fc36e
Gabriel MELOCHE <g.meloche@student.isartdigital.com>
2018-11-27 15:53:25 -0500

    vec4 now compiles

--- include/Math/Vector/Vec4.h
+++ include/Math/Vector/Vec4.h
@@ -1,23 +1,25 @@
 #pragma once
 #include <Math/Vector/Vec3.h>
 
-namespace Math
+namespace Math::Vector
 {
 	class Vec4
 	{
 	public:
-		float m_x;
-		float m_y;
-		float m_z;
-		float m_w;
+		float mf_x;
+		float mf_y;
+		float mf_z;
+		float mf_w;
 
-		Vec4(const Vec3& vec3, float p_w = 1.0f);
+		Vec4(const Vec3& p_vec3, float pf_w = 1.0f);
 		~Vec4() = default;
+		Vec4(const Vec4& p_other);
 
 		void Homogenize();
 		float GetMagnitude() const;
 		void Normalize();
-		static Vec4& operator+(const Vec4& p_Vec);
-		static Vec4& operator*(const float p_mult);
+		Vec4& operator+(const Vec4& p_v1);
+		//void operator*(const float pf_mult);
+		Vec4& operator*(const float pf_mult);
 	};
 }
--- src/Math/Vector/Vec4.cpp
+++ src/Math/Vector/Vec4.cpp
@@ -1,46 +1,72 @@
 #include <Math\Vector\Vec4.h>
+#include <Math\Vector\Vec3.h>
 #include <cmath>
 
-using namespace Math;
+using namespace Math::Vector;
 
-Vec4::Vec4(const Vec3& vec3, float p_w = 1.0f)
+Vec4::Vec4(const Vec3& p_vec3, float pf_w)
 {
-	m_x = vec3.x;
-	m_y = vec3.y;
-	m_z = vec3.z;
+	mf_x = p_vec3.mf_x;
+	mf_y = p_vec3.mf_y;
+	mf_z = p_vec3.mf_z;
+	mf_w = pf_w;
 }
-void Math::Vec4::Homogenize()
+
+Vec4::Vec4(const Vec4& p_other)
 {
-	if (m_w == 0)
+	mf_x = p_other.mf_x;
+	mf_y = p_other.mf_y;
+	mf_z = p_other.mf_z;
+	mf_w = p_other.mf_w;
+}
+
+void Vec4::Homogenize()
+{
+	if (mf_w == 0)
 		return;
 
-	m_x /= m_w;
-	m_y /= m_w;
-	m_z /= m_w;
+	mf_x /= mf_w;
+	mf_y /= mf_w;
+	mf_z /= mf_w;
 }
 
 float Vec4::GetMagnitude() const
 {
-	float x = m_x / m_w;
-	float y = m_y / m_w;
-	float z = m_z / m_w;
+	if (mf_w == 0.0f)
+		return 0.0f;
+
+	float x = mf_x / mf_w;
+	float y = mf_y / mf_w;
+	float z = mf_z / mf_w;
 	return sqrt(pow(x, 2)+ pow(y, 2) + pow(z, 2));
 }
 
 void Vec4::Normalize()
 {
 	float magn = GetMagnitude();
-	m_x /= magn;
-	m_y /= magn;
-	m_z /= magn;
+
+	if (magn == 0.0f)
+		return;
+
+	mf_x /= magn;
+	mf_y /= magn;
+	mf_z /= magn;
 }
 
-Vec4& Vec4::operator+(const Vec4 & p_Vec)
+Vec4& Vec4::operator+(const Vec4& p_v)
 {
-	
+	Vec4* p_tmp = new Vec4(Vec3(p_v.mf_x + mf_x,
+		p_v.mf_y + mf_y,
+		p_v.mf_z + mf_z));
+	return *p_tmp;
 }
 
-Vec4& Vec4::operator*(const float p_mult)
+Vec4& Vec4::operator*(const float pf_mult)
 {
-	// TODO: insert return statement here
+	float x = mf_x * pf_mult;
+	float y = mf_y * pf_mult;
+	float z = mf_z * pf_mult;
+	float w = mf_w * pf_mult;
+	Vec4* p_tmp = new Vec4(Vec3(x, y, z), w);
+	return *p_tmp;
 }

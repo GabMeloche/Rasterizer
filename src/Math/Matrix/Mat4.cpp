2d2f92c1f6ccc1ce43760375d18b7ce10b41d8a7
Lilian GADEAU <l.gadeau@student.isartdigital.com>
2018-11-27 15:05:20 -0500

    add mat4

---
+++ src/Math/Matrix/Mat4.cpp
@@ -0,0 +1,17 @@
+#include <Math/Matrix/Mat4.h>
+
+using namespace Math::Matrix::Mat4;
+
+//Mat4::Mat4()
+//{
+//}
+//
+//Mat4::~Mat4()
+//{
+//}
+
+Mat4& Mat4::operator*(Mat4 & p_mat1, Mat4 & p_mat2)
+{
+	// TODO: insert return statement here
+    p_mat1
+}

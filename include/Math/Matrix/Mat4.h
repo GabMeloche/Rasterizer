bf3220a63fe3e3b440ffe383cee983fb24b88ef0
Lilian GADEAU <l.gadeau@student.isartdigital.com>
2018-11-29 11:19:54 -0500

    add rotation and scale matrix

--- include/Math/Matrix/Mat4.h
+++ include/Math/Matrix/Mat4.h
@@ -15,10 +15,11 @@ namespace Math::Matrix
 
 		void SetMatrix(float p_Matrix[][4]);
 
-		Mat4& operator*(const Mat4& p_mat);
-		Vector::Vec4& operator*(const Math::Vector::Vec4& p_vec);
+		Mat4& operator*(const Mat4& p_Matrix);
+		Vector::Vec4& operator*(const Math::Vector::Vec4& p_Vector);
 
-		//static Mat4 CreateTransformMatrix(const Vector::Vec3& rotation, const Vector::Vec3& position, const Vector::Vec3& scale);
-		static Mat4 CreateTranslationMatrix(const Vector::Vec3& translation);
+    	static Mat4 CreateTranslationMatrix(const Vector::Vec3& p_Translation);
+		static Mat4 CreateScaleMatrix(const Vector::Vec3& p_Scale);
+		static Mat4 CreateRotationMatrix(const Vector::Vec3& p_Rotation);
     };
 }
--- src/Math/Matrix/Mat4.cpp
+++ src/Math/Matrix/Mat4.cpp
@@ -29,7 +29,7 @@ void Mat4::SetMatrix(float p_Matrix[][4])
 
 
 
-Mat4& Mat4::operator*(const Mat4 & p_mat)
+Mat4& Mat4::operator*(const Mat4 & p_Matrix)
 {
 	auto* TmpMat = new Mat4;
 
@@ -38,45 +38,82 @@ Mat4& Mat4::operator*(const Mat4 & p_mat)
         for (int j = 0; j < 4; j++)
         {
 			for (int k = 0; k < 4; k++)
-				TmpMat->mf_Matrice4[i][j] += this->mf_Matrice4[i][k] * p_mat.mf_Matrice4[k][j];
+				TmpMat->mf_Matrice4[i][j] += this->mf_Matrice4[i][k] * p_Matrix.mf_Matrice4[k][j];
         }
     }
 
 	return *TmpMat;
 }
 
-Math::Vector::Vec4& Mat4::operator*(const Math::Vector::Vec4& p_vec)
+Math::Vector::Vec4& Mat4::operator*(const Math::Vector::Vec4& p_Vector)
 {
 	auto* TmpVec = new Vector::Vec4;
 
 	for (int i = 0; i < 4; i++)
 	{
-		TmpVec->mf_x += this->mf_Matrice4[0][i] * p_vec.mf_x;
+		TmpVec->mf_x += this->mf_Matrice4[0][i] * p_Vector.mf_x;
 	}
 	for (int i = 0; i < 4; i++)
 	{
-		TmpVec->mf_y += this->mf_Matrice4[1][i] * p_vec.mf_y;
+		TmpVec->mf_y += this->mf_Matrice4[1][i] * p_Vector.mf_y;
 	}
     for (int i = 0; i < 4; i++)
 	{
-		TmpVec->mf_z += this->mf_Matrice4[2][i] * p_vec.mf_z;
+		TmpVec->mf_z += this->mf_Matrice4[2][i] * p_Vector.mf_z;
 	}
     for (int i = 0; i < 4; i++)
 	{
-		TmpVec->mf_w += this->mf_Matrice4[3][i] * p_vec.mf_w;
+		TmpVec->mf_w += this->mf_Matrice4[3][i] * p_Vector.mf_w;
 	}
     
     return *TmpVec;
 }
 
-Mat4 Math::Matrix::Mat4::CreateTranslationMatrix(const Vector::Vec3 & translation)
+Mat4 Math::Matrix::Mat4::CreateTranslationMatrix(const Vector::Vec3 & p_Translation)
 {
 	Mat4 TmpMat;
 
-	TmpMat.mf_Matrice4[0][3] = translation.mf_x;
-	TmpMat.mf_Matrice4[1][3] = translation.mf_y;
-	TmpMat.mf_Matrice4[2][3] = translation.mf_z;
+	TmpMat.mf_Matrice4[0][3] = p_Translation.mf_x;
+	TmpMat.mf_Matrice4[1][3] = p_Translation.mf_y;
+	TmpMat.mf_Matrice4[2][3] = p_Translation.mf_z;
 
 	return TmpMat;
+}
+
+Mat4 Math::Matrix::Mat4::CreateScaleMatrix(const Vector::Vec3 & p_Scale)
+{
+	Mat4 TmpMat;
+
+	TmpMat.mf_Matrice4[0][0] = p_Scale.mf_x;
+	TmpMat.mf_Matrice4[1][1] = p_Scale.mf_y;
+	TmpMat.mf_Matrice4[2][2] = p_Scale.mf_z;
+
+	return TmpMat;
+}
+
+Mat4 Math::Matrix::Mat4::CreateRotationMatrix(const Vector::Vec3 & p_Rotation)
+{
+	Mat4 TmpMatX;
+	Mat4 TmpMatY;
+	Mat4 TmpMatZ;
+
+	TmpMatX.mf_Matrice4[1][1] = cos(p_Rotation.mf_x);
+	TmpMatX.mf_Matrice4[1][2] = -sin(p_Rotation.mf_x);
+	TmpMatX.mf_Matrice4[2][1] = cos(p_Rotation.mf_x);
+	TmpMatX.mf_Matrice4[2][2] = sin(p_Rotation.mf_x);
+    
+	TmpMatY.mf_Matrice4[0][0] = cos(p_Rotation.mf_y);
+	TmpMatY.mf_Matrice4[0][2] = -sin(p_Rotation.mf_y);
+	TmpMatY.mf_Matrice4[2][0] = cos(p_Rotation.mf_y);
+	TmpMatY.mf_Matrice4[2][2] = sin(p_Rotation.mf_y);
+    
+	TmpMatZ.mf_Matrice4[0][0] = cos(p_Rotation.mf_z);
+	TmpMatZ.mf_Matrice4[0][1] = -sin(p_Rotation.mf_z);
+	TmpMatZ.mf_Matrice4[1][0] = cos(p_Rotation.mf_z);
+	TmpMatZ.mf_Matrice4[1][1] = sin(p_Rotation.mf_z);
+
+	Mat4 TmpMatTotal = TmpMatX * TmpMatY * TmpMatZ;
+
+	return TmpMatTotal;
 }
 

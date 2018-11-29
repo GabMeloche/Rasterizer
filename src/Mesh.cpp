633cd9dc19ce14add57a63cbccf66912d4c213cc
Gabriel MELOCHE <g.meloche@student.isartdigital.com>
2018-11-29 16:18:06 -0500

    Mesh: finished cube, almost finished sphere. Missing Indices initialization in both

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -1,6 +1,8 @@
+#define _USE_MATH_DEFINES
 #include <Mesh.h>
 #include <Vertex.h>
 #include <vector>
+#include <cmath>
 
 Mesh* Mesh::CreateCube()
 {
@@ -62,6 +64,19 @@ Mesh* Mesh::CreateCube()
 
 Mesh * Mesh::CreateSphere(int pi_latitudeCount, int pi_longitudeCount)
 {
+	Mesh* mesh = new Mesh;
 
-	return nullptr;
+	for (int i = 0; i < pi_latitudeCount; ++i)
+	{
+		for (int j = 0; j < pi_longitudeCount; ++j)
+		{
+			Vertex v;
+			float x = sin(M_PI * j / pi_longitudeCount) * cos(2 * M_PI * i / pi_latitudeCount);
+			float y = sin(M_PI * j / pi_longitudeCount) * sin(2 * M_PI * i / pi_latitudeCount);
+			float z = cos(M_PI * j / pi_longitudeCount);
+			v.m_position = { x, y, z };
+			mesh->m_vertices.emplace_back(v);
+		}
+	}
+	return mesh;
 }

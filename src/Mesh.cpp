8892ca6b1e3e9c69c72ab6ba8cbc916445ba913e
Gabriel MELOCHE <g.meloche@student.isartdigital.com>
2018-11-29 15:52:28 -0500

    Mesh: finished CreateCube. Sphere not started

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -5,23 +5,63 @@
 Mesh* Mesh::CreateCube()
 {
 	Mesh* mesh = new Mesh;
-	for (float i = 0.0f; i < 2.0f; ++i)
-	{
-		mesh->m_vertices.emplace_back(Vertex{ Vec3{-0.5f, 0.5f, i} });
-		mesh->m_vertices.emplace_back(Vertex{ Vec3{0.5f, 0.5f, i} });
-		mesh->m_vertices.emplace_back(Vertex{ Vec3{-0.5f, -0.5f, i} });
-		mesh->m_vertices.emplace_back(Vertex{ Vec3{0.5f, -0.5f, i} });
-		mesh->m_triangles.emplace_back(Triangle{ mesh->m_vertices[0], 
-												mesh->m_vertices[1], 
-												mesh->m_vertices[2] });
-		mesh->m_triangles.emplace_back(Triangle{ mesh->m_vertices[1],
-												mesh->m_vertices[2],
-												mesh->m_vertices[3] });
-	}
+
+	mesh->m_vertices.emplace_back(Vertex{ Vec3{-0.5f, 0.5f, 0.0f} });
+	mesh->m_vertices.emplace_back(Vertex{ Vec3{0.5f, 0.5f, 0.0f} });
+	mesh->m_vertices.emplace_back(Vertex{ Vec3{-0.5f, -0.5f, 0.0f} });
+	mesh->m_vertices.emplace_back(Vertex{ Vec3{0.5f, -0.5f, 0.0f} });
+	mesh->m_vertices.emplace_back(Vertex{ Vec3{ -0.5f, 0.5f, 1.0f } });
+	mesh->m_vertices.emplace_back(Vertex{ Vec3{ 0.5f, 0.5f, 1.0f } });
+	mesh->m_vertices.emplace_back(Vertex{ Vec3{ -0.5f, -0.5f, 1.0f } });
+	mesh->m_vertices.emplace_back(Vertex{ Vec3{ 0.5f, -0.5f, 1.0f } });
+
+	mesh->m_triangles.emplace_back(Triangle{ mesh->m_vertices[0], 
+											mesh->m_vertices[1], 
+											mesh->m_vertices[2] });
+	mesh->m_triangles.emplace_back(Triangle{ mesh->m_vertices[1],
+											mesh->m_vertices[2],
+											mesh->m_vertices[3] }); //front 
+
+	mesh->m_triangles.emplace_back(Triangle{ mesh->m_vertices[0],
+		mesh->m_vertices[1],
+		mesh->m_vertices[4] });
+	mesh->m_triangles.emplace_back(Triangle{ mesh->m_vertices[1],
+		mesh->m_vertices[4],
+		mesh->m_vertices[5] }); //top
+
+	mesh->m_triangles.emplace_back(Triangle{ mesh->m_vertices[0], 
+		mesh->m_vertices[2],
+		mesh->m_vertices[4] });
+	mesh->m_triangles.emplace_back(Triangle{ mesh->m_vertices[2],
+		mesh->m_vertices[4],
+		mesh->m_vertices[6] }); //left
+
+	mesh->m_triangles.emplace_back(Triangle{ mesh->m_vertices[5],
+		mesh->m_vertices[4],
+		mesh->m_vertices[6] });
+	mesh->m_triangles.emplace_back(Triangle{ mesh->m_vertices[5],
+		mesh->m_vertices[6],
+		mesh->m_vertices[7] }); //back
+
+	mesh->m_triangles.emplace_back(Triangle{ mesh->m_vertices[5],
+		mesh->m_vertices[1],
+		mesh->m_vertices[7] });
+	mesh->m_triangles.emplace_back(Triangle{ mesh->m_vertices[1],
+		mesh->m_vertices[7],
+		mesh->m_vertices[3] }); //right
+
+	mesh->m_triangles.emplace_back(Triangle{ mesh->m_vertices[3],
+		mesh->m_vertices[2],
+		mesh->m_vertices[7] });
+	mesh->m_triangles.emplace_back(Triangle{ mesh->m_vertices[2],
+		mesh->m_vertices[7],
+		mesh->m_vertices[6] }); //bottom
+
 	return mesh;
 }
 
 Mesh * Mesh::CreateSphere(int pi_latitudeCount, int pi_longitudeCount)
 {
+
 	return nullptr;
 }

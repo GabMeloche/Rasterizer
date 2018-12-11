b1592a4cdd8869c35c3e02ccecff04257d2790b8
Gabriel MELOCHE <g.meloche@student.isartdigital.com>
2018-12-11 13:47:20 -0500

    merge fix

--- src/Rasterizer.cpp
+++ src/Rasterizer.cpp
@@ -1,6 +1,7 @@
 #include <Rasterizer.h>
 #include <Vertex.h>
 #include <Triangle.h>
+#include <Math/Vector/Vec3.h>
 #include <iostream>
 
 Rasterizer::Rasterizer()
@@ -41,8 +42,9 @@ void Rasterizer::RenderScene(Scene* p_scene, Texture& p_Target, SDL_Renderer* p_
 	float v2y;
 	float v3x;
 	float v3y;
+	size_t eSize = p_scene->getEntities().size();
 
-	for (int i = 0; i < p_scene->getEntities().size(); i++)
+	for (int i = 0; i < eSize; i++)
 	{
 		Triangle pos = p_scene->getEntities()[i]->getMesh()->getTriangles()[0];
 		v1x = pos.m_v1.m_posMatrix->mf_Matrice4[0][0];
@@ -63,7 +65,6 @@ void Rasterizer::RenderScene(Scene* p_scene, Texture& p_Target, SDL_Renderer* p_
 
 		float curx1 = v1x;
 		float curx2 = v1x;
-		ZBuffer(&p_Target, p_scene);
 
 		for (int scanlineY = v1y; scanlineY <= v2y; scanlineY++)
 		{
@@ -86,7 +87,7 @@ void Rasterizer::RenderScene(Scene* p_scene, Texture& p_Target, SDL_Renderer* p_
 			//std::cout << p_Target.m_pixels[x0 + y0 * p_Target.mui_w].ucm_r << '\n';
 			while(true)
 			{
-
+				ZBuffer(x0, y0, pos, p_Target);
 				/*p_Target.m_pixels[x0 + y0 * m_width].ucm_r = (int)-sqrt(pow(v1x - x0, 2) + pow(v1y - y0, 2)) / 2;
 				p_Target.m_pixels[x0 + y0 * m_width].ucm_g = (int)-sqrt(pow(v2x - x0, 2) + pow(v2y - y0, 2)) / 2;
 				p_Target.m_pixels[x0 + y0 * m_width].ucm_b = (int)-sqrt(pow(v3x - x0, 2) + pow(v3y - y0, 2)) / 2;*/
@@ -121,16 +122,67 @@ void Rasterizer::RenderScene(Scene* p_scene, Texture& p_Target, SDL_Renderer* p_
 	}
 }
 
-void Rasterizer::ZBuffer(Texture* p_texture, Scene* p_scene)
-{
-	std::vector<Entity*>& allEntities = p_scene->getEntities();
-	size_t size = allEntities.size();
+//TODO: ZBuffer must take a single pixel as parameter, and must be called to draw every pixel (not the whole screen at once)
 
-	for (size_t i = 0; i < size; ++i)
+void Rasterizer::ZBuffer(unsigned int p_x, unsigned int p_y, Triangle& p_triangle, Texture& p_texture)
+{
+	static unsigned int color = 0;
+	Vec3 v0 = { p_triangle.m_v2.m_position - p_triangle.m_v1.m_position };
+	Vec3 v1 = { p_triangle.m_v3.m_position - p_triangle.m_v1.m_position };
+
+	float u = v0.GetMagnitude();
+	float v = v1.GetMagnitude();
+
+	Vec3 vMult0 = v0 * u;
+	Vec3 vMult1 = v1 * v;
+
+	Vec3 Point = p_triangle.m_v1.m_position;
+	Point = Point + vMult1;
+
+	if (m_zBuffer[p_x][p_y] < Point.mf_z)
+	{
+		m_zBuffer[p_x][p_y] = Point.mf_z;
+		p_texture.SetPixelColor(p_x, p_y, { color, 0, 0, 255 });
+		++color;
+		
+		if (color == 255)
+			color = 0;
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*std::vector<Entity*>& allEntities = p_scene->getEntities();
+	size_t eSize = allEntities.size();
+	
+	for (size_t i = 0; i < eSize; ++i)
+	{
+		const std::vector<Triangle>& triangles = allEntities[i]->getMesh()->getTriangles();
+
+		for (size_t j = 0; j < triangles.size(); ++j)
+		{
+			for (unsigned int k = 0; k < 3; ++k)
+			{
+				//'[]' operator overloaded on triangles to return 1 of 3 vertices of triangle
+				if (triangles[j][k].m_position.mf_z > m_zBuffer[p_x][p_y])
+				{
+					m_zBuffer[p_x][p_y] = triangles[j][k].m_position.mf_z;
+				}
+			}
+
+		}
+	}
+
+	/*for (size_t i = 0; i < size; ++i)
 	{
 		float currDepth = allEntities[i]->getMesh()->getTriangles()[0].m_v1.m_position.mf_z;
 		float* triangleZone = allEntities[i]->CheckTriangleZone();
-
 
 		for (size_t j = triangleZone[0]; j < triangleZone[1]; ++j)
 		{
@@ -148,5 +200,4 @@ void Rasterizer::ZBuffer(Texture* p_texture, Scene* p_scene)
 				}
 			}
 		}
-	}
-}
+	}*/

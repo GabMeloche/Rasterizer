48f2229ccc4b3ed57c4ea3d2585d42ec7341d599
Victor JORE <v.jore@student.isartdigital.com>
2018-12-17 15:00:22 -0500

    Error fix, .h uploaded

--- include/Rasterizer.h
+++ include/Rasterizer.h
@@ -17,6 +17,6 @@ public:
 	void ZBuffer(unsigned int p_x, unsigned int p_y, Triangle & p_triangle, Texture& p_texture);
 	Vec3 GetPixelPos(Vec4 & p_v);
 	void ClearZBuffer();
-	void FillTriangles(Vec3& v1, Vec3& v2, Vec3& v3, int top);
+	void FillTriangles(Vec3& v1, Vec3& v2, Vec3& v3);
 };
 
